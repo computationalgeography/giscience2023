0bf6c82fbbf9ee40f23d349ba461cd371aaf467d
Oliver Schmitz <o.schmitz@uu.nl>
2023-09-10 11:30:52 +0200

    Notebook added

---
+++ notebook/utils/__init__.py
@@ -0,0 +1,9 @@
+import matplotlib as mpl
+
+mpl.rcParams["figure.dpi"] = 150
+
+from .generate_inputs import *
+from .plot_luminosity import *
+from .plot_population import *
+from .plot_temperature import *
+from .animate import *
